1979f0b778ad805a89aa505eae4bbf831bee8648
schwarzflugel <weibflugel@naver.com>
2016-07-16 18:18:12 +0900

    0716_1818

--- wiki.js
+++ wiki.js
@@ -5,3 +5,25 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/wikiDB');
 var wikiDB = mongoose.connection;
+wikiDB.once('open', function(){
+	console.log('DB connected');
+});
+wikiDB.on('error', function(res){
+	res.redirect('/dbError');
+});
+//파싱을 위한 모듈 추가
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+//
+var session = require('express-session');
+var flash = require('connect-flash');
+//
+var path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function(){
+	console.log('Server on');
+});
