50ca5bdc349a7c75149c1a947aea098a9a2b46b3
schwarzflugel <weibflugel@naver.com>
2016-07-17 20:22:41 +0900

    replace_link

--- function/tag.js
+++ function/tag.js
@@ -3,8 +3,19 @@ module.exports = function(content){
 
 	docContent = docContent.replace(/\'\'\'([^\']*)\'\'\'/g,
 	'<strong class=\"content_strong\">$1</strong>');	//'''$1'''
+	docContent = docContent.replace(/=======([^=]*)=======/g,
+		"<h6 class=\"outline\">$1</h6>");	//===$1===
+	docContent = docContent.replace(/======([^=]*)======/g,
+		"<h5 class=\"outline\">$1</h5>");	//===$1===
+	docContent = docContent.replace(/=====([^=]*)=====/g,
+		"<h4 class=\"outline\">$1</h4>");	//===$1===
+	docContent = docContent.replace(/====([^=]*)====/g,
+		"<h3 class=\"outline\">$1</h3>");	//===$1===
+	docContent = docContent.replace(/===([^=]*)===/g,
+		"<h2 class=\"outline\">$1</h2>");	//===$1===
 	docContent = docContent.replace(/==([^=]*)==/g,
-	"<h1 class=\"outline\">$1</h1>");	//==$1==
-
+		"<h1 class=\"outline\">$1</h1>");	//==$1==
+	docContent = docContent.replace(/\[\[(((?!\[\[).)*)\]\]/g,
+		"<a href=\"/view/$1\">$1</a>");
 	return docContent;
 };
